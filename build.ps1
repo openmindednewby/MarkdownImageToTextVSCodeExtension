9c2be60ab9cd64f6cfe92bf76aee050d6d38b922
Demetrios Loizides <loizidesdemetris@gmail.com>
2025-08-02 23:23:47 +0300

    updated build file

--- build.ps1
+++ build.ps1
@@ -11,6 +11,29 @@ if (Test-Path $OutDir) {
 Write-Host "Running 'npm run compile'..."
 npm run compile
 
+# Copy needed node_modules folders to out/node_modules to make sure Tesseract.js can find its files
+$nodeModulesToCopy = @(
+    "tesseract.js",
+    "tesseract.js-core",
+    "@tesseract.js-data"
+)
+
+foreach ($module in $nodeModulesToCopy) {
+    $sourcePath = Join-Path -Path "node_modules" -ChildPath $module
+    $destPath = Join-Path -Path $OutDir -ChildPath ("node_modules\" + $module)
+
+    if (Test-Path $sourcePath) {
+        Write-Host "Copying $module from $sourcePath to $destPath ..."
+        # Ensure destination directory exists
+        if (-not (Test-Path $destPath)) {
+            New-Item -ItemType Directory -Path $destPath -Force | Out-Null
+        }
+        Copy-Item -Recurse -Force $sourcePath\* $destPath
+    } else {
+        Write-Warning "$module not found in node_modules, skipping..."
+    }
+}
+
 Write-Host "Running 'npm version patch'..."
 npm version patch
 
